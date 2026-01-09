9de3ee1169aec7f16c7ef1debd1680414d815446
Alexis Arriola <20253ds138@utez.edu.mx>
2026-01-09 15:16:26 -0600

    Proyecto 01: hola mundo

---
+++ proyecto-01-hola-mundo/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main ( String[]args){
+        System.out.println("Hello World");
+        System.out.println("Alexis Arriola");
+        System.out.println("9/01/26");
+        System.out.println("2E");
+        System.out.println("texto: Hola mundo");
+
+
+
+    }
+}
